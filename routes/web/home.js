5215e49c41aa96d881763829a5f2bb0105c20785
nick <potto010@umn.edu>
2020-11-23 05:22:28 -0600

    its gettin there

--- routes/web/home.js
+++ routes/web/home.js
@@ -2,14 +2,28 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req,res){
-    console.log("Hello I'm on the START page!");
     res.render("pages/index");
     
 });
 
 router.get("/home", function(req,res){
-    console.log("Hello I'm on the HOME page!");
     res.render("pages/home");
+});
+
+router.get("/login", function(req,res){
+    res.render("pages/login");
+});
+
+router.get("/signup", function(req,res){
+    res.render("pages/signup");
+});
+
+router.get("/study", function(req,res){
+    res.render("pages/study");
+});
+
+router.get("/quiz", function(req,res){
+    res.render("pages/quiz");
 });
 
 module.exports = router;
